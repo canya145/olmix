d6eefda8e347333d3dda6cfdb35dc432688151e2
Artem Dushko <adushko@copyright.com>
2022-12-27 17:44:49 +0200

    Implement login API logic

--- src/server/controllers/users.js
+++ src/server/controllers/users.js
@@ -31,3 +31,18 @@ exports.login = function (req, res, next) {
     .then((response) => writeResponse(res, response))
     .catch(next);
 };
+
+exports.login = function (req, res, next) {
+  const {email, password} = req.body;
+
+  if (!email) {
+    throw {email: 'This field is required.', status: 400};
+  }
+  if (!password) {
+    throw {password: 'This field is required.', status: 400};
+  }
+
+  Users.login(dbUtils.getSession(), email, password)
+    .then((response) => writeResponse(res, response))
+    .catch(next);
+};
